d540d692b3bc7e27a13197665cd4c66d33417c23
jeanlucqs <ojeanlucas.br@gmail.com>
2024-10-25 10:18:53 -0300

    aula 25/10

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/<int:post_id>', views.post, name='post'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('contato/', views.contato, name='contato'),
+
 ]
+
+
 
